875483078b0944d62407b3220d86fb7b45c67236
Shreyas Srinivasan <shreyass@mit.edu>
2021-12-23 21:27:47 -0500

    Adjust to base poker actions

--- cpp_skeleton/libs/skeleton/include/skeleton/actions.h
+++ cpp_skeleton/libs/skeleton/include/skeleton/actions.h
@@ -7,16 +7,12 @@
 namespace pokerbots::skeleton {
 
 struct Action {
-  enum Type { FOLD, CALL, CHECK, RAISE, ASSIGN };
+  enum Type { FOLD, CALL, CHECK, RAISE };
 
   Type actionType;
   int amount;
-  std::array<Card, 2> cards;
 
   Action(Type t = Type::CHECK, int a = 0) : actionType(t), amount(a) {}
-
-  Action(Type t, std::array<Card, 2> cards)
-      : actionType(t), amount(0), cards(std::move(cards)) {}
 
   friend std::ostream &operator<<(std::ostream &os, const Action &a);
 };
--- cpp_skeleton/libs/skeleton/src/actions.cpp
+++ cpp_skeleton/libs/skeleton/src/actions.cpp
@@ -7,10 +7,6 @@ namespace pokerbots::skeleton {
 
 std::ostream &operator<<(std::ostream &os, const Action &a) {
   switch (a.actionType) {
-  case Action::Type::ASSIGN:
-    fmt::print(os, FMT_STRING("A{}"),
-               fmt::join(a.cards.begin(), a.cards.end(), ","));
-    return os;
   case Action::Type::FOLD:
     return os << 'F';
   case Action::Type::CALL:
