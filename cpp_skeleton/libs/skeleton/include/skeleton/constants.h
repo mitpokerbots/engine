48975c4e4d1713e5d4d17151a24ef2dd8fa23c18
Shreyas Srinivasan <shreyass@mit.edu>
2021-12-23 21:31:53 -0500

    Update constants for base poker

--- cpp_skeleton/libs/skeleton/include/skeleton/constants.h
+++ cpp_skeleton/libs/skeleton/include/skeleton/constants.h
@@ -7,10 +7,9 @@
 namespace pokerbots::skeleton {
 
 inline constexpr int NUM_ROUNDS = 1000;
-inline constexpr int STARTING_STACK = 200;
+inline constexpr int STARTING_STACK = 400;
 inline constexpr int BIG_BLIND = 2;
 inline constexpr int SMALL_BLIND = 1;
-inline constexpr int NUM_BOARDS = 3;
 
 using Card = std::string;
 using Pip = int;
